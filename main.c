e25a98eac57271065af653b1fcb65806f0ccead4
pratik0007 <pratikgondane07@gmail.com>
2022-11-25 14:54:43 +0530

    c practise last seen

--- main.c
+++ main.c
@@ -8,8 +8,8 @@ int main(){
     printf("\nArray initilazation code\n");
     printf("\nType a number which will set array size\n");
     scanf("%d",&x);
-    z=&x;
     int arr[x];
+    z=&arr[x];
     printf("\n Array valuse  set : %d\n", x);
     printf("\n for Insert 1. \t For travel asc press 2. \t For travel desc type 3.\n");
     scanf("%d",&y);
@@ -18,9 +18,9 @@ int main(){
         for(int i=0;i<x;i++){
             printf("\n This section is for insertion ");
             printf("\n For postion %d Enter value : ",i);
-            scanf("%d",arr);
-            arr[i+1];
-            printf("\n Value in array : %d \n",arr[1+1]);
+            scanf("%d",(arr+i));
+
+            printf("\n Value in array : %d \n",*(arr+i));
         }
         break;
         case 2: printf("case 2 selected\n");
@@ -28,15 +28,15 @@ int main(){
                 printf("\n This section is for Travelsing the array");
                 // printf("\n For postion %d Enter value : ",i);
                 // scanf("%d",arr+0);
-                printf("\n Value in array : %d \n",arr[x+i]);
+                printf("\n Value in array : %ls \n",*(arr+i));
         }
         break;
         case 3: printf("case 3 selected\n");
-        for(int i=0;i<x;i++){
+        for(int i=5;i>=x;i--){
             printf("\n This section is for Travelsing the array descending order ");
             // printf("\n For postion %d Enter value : ",i);
             // scanf("%d",arr+0);
-            printf("\n Value in array : %d \n",arr[x-i]);
+            printf("\n Value in array : %d \n",(arr-i));
         }
         break;
 
