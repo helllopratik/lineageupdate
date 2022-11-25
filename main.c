217600a18ff62c79579f2848b8d055bbadac0ffc
pratik0007 <pratikgondane07@gmail.com>
2022-11-25 11:37:57 +0530

    upload via VSCODE

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+int sum(),sub();
+int main(){
+    printf("Hello world\n"); 
+    sum();
+    sub();
+    return 0;
+};
+
+int sum(){
+    printf("Sum function Working\n");
+    return 0;
+};
+int sub(){
+    printf("sub function working\n");
+    return 0;
+}
