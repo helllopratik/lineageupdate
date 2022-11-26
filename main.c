c3e83f8a09d903e156054dee1f7d7b2aa1c25386
pratik0007 <pratikgondane07@gmail.com>
2022-11-26 19:44:48 +0530

    c practise last seen

--- main.c
+++ main.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include <math.h>
-
+#include <stdlib.h>
 void stack(),printNumber(int arr[],int n);
 
 int main(){
     printf("Array Printing Number");
     int arr[] = {1,2,3,4,5,6};
-    printNumber(arr,6);
+    printNumber(arr,9);
     return 0;
 };
 void printNumber(int arr[],int n){
     for(int i=0;i<n;i++){
-        printf("%d \t",arr[i]);
-        if(i==0){
-            printNumber(arr,10);
-        }
-        if(i==7)
-        stack();
-        
+        printf("\t %d \n",arr[i]);
+        // if(i==0){
+        //     break;
+        // }
+        // if(i==7){
+        // stack();
+        // }
     }
 };
 void stack(){
-    printf("Call By value");
+    printf("Call By value\n");
+    exit(0);
 }
