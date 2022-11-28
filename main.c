3241e7d94a77eaa896d0bc033b75fd59e92bb378
pratik0007 <pratikgondane07@gmail.com>
2022-11-28 21:13:22 +0530

    c practise last seen

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-void reverse(int arr[],int n);
+void reverse(int arr[],int f);
 int main(){
-    int arr[] = {1,2,3,4,5,6,7,8,9,10,11};
-    printf("reverse an array\n");
-    reverse(arr,10);
-    return 0;
+    int arr[] = {1,2,3,4,5,6};
+    reverse(arr,6);
+    printf("\n");
 }
-void reverse (int arr[],int n){
-        for(int i=0;i<=n;i++){
-        printf("%d ",arr[n-i]);
-        }
-        printf("\n");
-        }
+void reverse(int arr[],int f){
+    for(int i=0;i<=f/2;i++){
+        int q = arr[i];
+        int w = arr[f-i-1];
+        w=arr[i];
+        q=arr[f-i-1];
+        printf("%d, %d",w,q);
+    }
+
+}
