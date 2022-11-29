0f23923e7c656d19d6bb08d7f286ae074035b942
pratik0007 <pratikgondane07@gmail.com>
2022-11-29 17:19:54 +0530

    c practise last seen

--- main.c
+++ main.c
@@ -1,39 +1,16 @@
 #include <stdio.h>
-#include <math.h>
 
-int sum(int arr[],int n);
-int show(int arr[],int n);
-int x,y,z;
-int main();
-int arr[10];
-int main(){
-        printf("Choosen value 1 \n");
-        printf("Total size of array : ");
-        scanf("%d",&y);
-        sum(arr,y);
-    
-    return 0;
+int main (){
+    int x,y,z;
+    printf("number for fib: (n>2) ");
+    scanf("%d",&z);
+int fib[z];
+    fib[0] = 0;
+    fib[1] = 1;
+for(int i = 2;i<z;i++){
+    fib[i] = fib[i-1] + fib[i-2];
+    printf("\n%d\t \n",fib[i]);
+
 }
-int sum(int arr[],int n){
-
-    for(int i =0;i<n;i++){
-        printf("Enter value for index i = %d : ",i);
-        scanf("%d",&arr[n-i+1]);
-        // printf("%d\n",arr[n-i+1]);
-    }
-    for(int i =0;i<n;i++){
-        int z = arr[(i+1)+(i+2)];
-       printf("%d",arr[i]);
-    }
-    printf("\n***********************\n");
-    // show(arr,n);
-
-    
-}
-
-int show(int arr[],int n){
-    for(int t=0;t<n;t++){
-        printf("%d\n",arr[n-t+1]);
-    }
-    return 0;
+return 0;
 }
--- touch.c
+++ touch.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
-#include <math.h>
 
-int main()
-{
-int x,y,z;
-printf("C language in program example in one file example");
-printf("\nEnter value for X: ");
-scanf("%d",&x);
-printf("\nEnter Value  for Y: ");
-scanf("%d",&y);
-printf("\nEnter Value for Z: ");
-scanf("%d",&z);
-printf("This are the value assigned by the user input \n For X =  %d\n For Y =  %d\n For Z = %d\n",x, y, z);
-printf("Swaping program");
-int temp = x;
-x = y;
-y = temp;
-printf("Swapped Value is as follows : For X = %d\n For Y = %d\n For Z = %d\n ",x, y, z);
+int main (){
+    int x,y,z;
+    printf("number for fib: (n>2) ");
+    scanf("%d",&z);
+int fib[z];
+    fib[0] = 0;
+    fib[1] = 1;
+for(int i = 2;i<z;i++){
+    fib[i] = fib[i-1] + fib[i-2];
+    printf("\n%d\t \n",fib[i]);
+
+}
 return 0;
 }
