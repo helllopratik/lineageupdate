45738be8d2635abc30992a80694cb3f987425416
pratik0007 <pratikgondane07@gmail.com>
2022-12-05 19:56:48 +0530

    c practise last seen

--- main.c
+++ main.c
@@ -1,13 +1,41 @@
+#include <string.h>
 #include <stdio.h>
+void show();
+struct student{
+    int rollno;
+    char name[100];
+};
 
 int main(){
-    char arr[50];
-    printf("Enter your name: ");
-    fgets(arr,50,stdin);
-    puts(arr);
-    printf("\n");
-    printf("Your name %s",arr);
+    struct student s1;
+    struct student s2;
+    struct student s3;
 
-    return 0;
+    strcpy(s1.name,"abc");
 
+    // 
+    printf("Enter Roll no: ");
+    scanf("%d",&s1.rollno);
+
+
+    strcpy(s2.name,"def");
+    printf("Enter Roll no: ");
+    scanf("%d",&s2.rollno);
+    
+    strcpy(s3.name,"abc");
+    printf("Enter Roll no: ");
+    scanf("%d",&s3.rollno);
+// 
+    printf("%s\t",s1.name);
+    printf("%d\n",s1.rollno);
+    // 
+    printf("%s\t",s2.name);
+    printf("%d\n",s2.rollno);
+
+    // 
+    printf("%s\t",s3.name);
+    printf("%d\n",s3.rollno);
+
+// 
+return 0;
 }
