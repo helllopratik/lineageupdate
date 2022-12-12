d39653f0817666e1076f1143339dd3457ee790c8
pratik0007 <pratikgondane07@gmail.com>
2022-12-12 19:44:16 +0530

    gui in c

--- test.c
+++ test.c
@@ -1,9 +1,30 @@
-#include <stdio.h>
-void solve() {
-    int x = printf("Hello");
-    printf(" %d\n", x);
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
+    LPSTR lpCmdLine, int nCmdShow)
+{
+    MessageBox(NULL, "Goodbye, cruel world!", "Note", MB_OK);
+    return 0;
 }
-int main() {
-	solve();
-	return 0;
+
+// Include gtk
+// #include <gtk/gtk.h>
+
+static void on_activate (GtkApplication *app) {
+  // Create a new window
+  GtkWidget *window = gtk_application_window_new (app);
+  // Create a new button
+  GtkWidget *button = gtk_button_new_with_label ("Hello, World!");
+  // When the button is clicked, close the window passed as an argument
+  g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_window_close), window);
+  gtk_window_set_child (GTK_WINDOW (window), button);
+  gtk_window_present (GTK_WINDOW (window));
+}
+
+int main (int argc, char *argv[]) {
+  // Create a new application
+  GtkApplication *app = gtk_application_new ("com.example.GtkApplication",
+                                             G_APPLICATION_FLAGS_NONE);
+  g_signal_connect (app, "activate", G_CALLBACK (on_activate), NULL);
+  return g_application_run (G_APPLICATION (app), argc, argv);
 }
