11fb8ba76e542408d48167009ea53b333fc15776
pratik0007 <pratikgondane07@outlook.com>
2023-09-29 21:51:19 +0530

    29-sept-2023

--- pointer/pointer.c
+++ pointer/pointer.c
@@ -1,28 +1,61 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<errno.h>
 
-int main(){
-    FILE *fptr;
-    char name[100];
-    int age;
-    float salary;
-    fptr = fopen("note.txt","w");
-    if(fptr == NULL){
-        printf("File Does not exist\n");
-        return 0;
+/* 
+    to count the total number of characters 
+    inside the source file
+*/
+long count_characters(FILE *);
+
+void main()
+{
+    int i;
+    long cnt;
+    char ch, ch1;
+    FILE *fp1, *fp2;
+
+    if (fp1 = fopen("note.txt", "r"))
+    {
+        printf("The FILE has been opened...\n");
+        fp2 = fopen("File_2.txt", "w");
+        cnt = count_characters(fp1);
+
+        /*
+            Make the pointer fp1 to point at the
+            last character of the file
+        */
+        fseek(fp1, -1L, 2);
+        printf("Number of characters to be copied %d\n", ftell(fp1));
+
+        while (cnt)
+        {
+            ch = fgetc(fp1);
+            fputc(ch, fp2);
+            fseek(fp1, -2L, 1); // shifts the pointer to the previous character
+            cnt--;
+        }
+        printf("\n**File copied successfully in reverse order**\n");
     }
-    printf("\nEnter the name: ");
-    scanf("%s",name);
-    fprintf(fptr,"name = %s\n",name);
-    printf("\nEnter age for %S: ",name);
-    scanf("%d",&age);
-    fprintf(fptr,"age = %d\n",age);
-    printf("\nEnter the salary for name:%s -> ",name);
-    scanf("%f",&salary);
-    fprintf(fptr,"salary = %f\n",salary);
-    fclose(fptr);
+    else
+    {
+        perror("Error occured\n");
+    }
+    fclose(fp1);
+    fclose(fp2);
+}
 
-
-
-
-    return 0;
+/*
+    Count the total number of characters in the file
+    that *f points to
+*/
+long count_characters(FILE *f)
+{
+    fseek(f, -1L, 2);
+    /*
+        returns the position of the 
+        last element of the file
+    */
+    long last_pos = ftell(f);
+    last_pos++;
+    return last_pos;
 }
