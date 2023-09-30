bae0a0ef8c231149d7a49e9c3a20c8dcc4d17e29
HelloPratik <38004222+pratik0007@users.noreply.github.com>
2023-09-30 21:42:47 +0530

    30-sept-2024

---
+++ basic.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int a;
+    int b;
+    printf("Enter value a: ");
+    cin>>a;
+    printf("Enter value b: ");
+    cin>>b;
+    
+    printf("hello world : %d\n",a);
+    cout<<"hello world with cout: "<<b<<endl;
+    return 0;
+}
